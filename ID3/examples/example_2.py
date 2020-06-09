b9ee545e91e369787c625e76810c913d4dd19387
Jan Piotrowski <j.piotrowski@millionmonkeys.pl>
2020-06-09 18:18:15 +0200

    Change example_2 for smaller sets

--- ID3/examples/example_2.py
+++ ID3/examples/example_2.py
@@ -19,27 +19,29 @@ def example_2():
     def curried_get_averaged_results_of_k_cross_validation_for_dataset(df):
         return get_averaged_results_of_k_cross_validation(df, target_attribute, k_min, k_max)
 
-    results_for_full_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(dataset)
+    dataset = shuffle(dataset)
+    one_eight_of_dataset = np.array_split(dataset, 8)[0]
+    one_eight_for_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(one_eight_of_dataset)
 
     dataset = shuffle(dataset)
-    half_of_dataset = np.array_split(dataset, 2)[0]
-    results_for_half_of_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(half_of_dataset)
+    one_sixteen_of_dataset = np.array_split(dataset, 16)[0]
+    one_sixteen_of_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(one_sixteen_of_dataset)
 
     dataset = shuffle(dataset)
-    one_third_of_dataset = np.array_split(dataset, 3)[0]
-    results_for_one_third_of_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(one_third_of_dataset)
+    one_thirty_two_of_dataset = np.array_split(dataset, 32)[0]
+    one_thirty_two_of_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(one_thirty_two_of_dataset)
 
     dataset = shuffle(dataset)
-    quarter_of_dataset = np.array_split(dataset, 4)[0]
-    results_for_quarter_of_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(quarter_of_dataset)
+    one_sixty_four_of_dataset = np.array_split(dataset, 64)[0]
+    one_sixty_four_of_dataset = curried_get_averaged_results_of_k_cross_validation_for_dataset(one_sixty_four_of_dataset)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     x = [i for i in range(k_min, k_max+1)]
-    plt.plot(x, results_for_full_dataset, 'r^')
-    plt.plot(x, results_for_half_of_dataset, 'go')
-    plt.plot(x, results_for_one_third_of_dataset, 'bs')
-    plt.plot(x, results_for_quarter_of_dataset, 'rs')
+    plt.plot(x, one_sixty_four_of_dataset, 'r^')
+    plt.plot(x, one_eight_for_dataset, 'go')
+    plt.plot(x, one_sixteen_of_dataset, 'bs')
+    plt.plot(x, one_thirty_two_of_dataset, 'rs')
 
     plt.xlabel('k in k-cross validation')
     plt.ylabel('accuracy of model')

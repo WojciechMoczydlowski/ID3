4a4ab01af4987d60886e8fe20b18ababb710c518
Jan Piotrowski <janfpiotrowski@gmail.com>
2020-06-09 22:14:55 +0200

    Update README.MD

--- ID3/main.py
+++ ID3/main.py
@@ -1,4 +1,7 @@
 # author: Jan Piotrowski, Wojciech Moczydlowski
+import sys
+sys.path.insert(0, "/home/janek/Desktop/PSZT/ID3/")
+
 from ID3.examples.example_1 import example_1
 from ID3.examples.example_2 import example_2
 
